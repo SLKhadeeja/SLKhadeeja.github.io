c72d42dcb223968554b441812a7df3d8b08c2a3f
Khadija Ladan <khadijaladan67@gmail.com>
2019-07-12 11:12:19 +0100

    added day of the week

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function updateClock(){
     var currentHours = currentTime.getHours();
     var currentMinutes = currentTime.getMinutes();
     var currentSeconds = currentTime.getSeconds();
-    var currentDay = currentTime.getDate();
+    var currentDate = currentTime.getDate();
     var currentMonth = currentTime.getMonth() + 1;
     var currentYear = currentTime.getFullYear();
+    var currentDay = currentTime.getDay();
 
     currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
     currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
@@ -14,11 +15,36 @@ function updateClock(){
     currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
     currentHours = (currentHours == 0) ? 12 : currentHours;
 
+    switch(currentDay){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday"            
+
+    }
+
     var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-    var currentDateString = currentDay + "/" + currentMonth + "/" + currentYear;
+    var currentDateString = currentDate + "/" + currentMonth + "/" + currentYear;
 
     document.getElementById('clock').innerHTML = currentTimeString;
-    document.getElementById('date').innerHTML = currentDateString
+    document.getElementById('date').innerHTML = currentDateString;
+    document.getElementById('day').innerHTML = day;
 
     setTimeout(updateClock, 1000);
 };
