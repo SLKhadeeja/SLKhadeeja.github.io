5e547806b1816fddda57b79839510e97041014a0
Khadija Ladan <khadijaladan67@gmail.com>
2019-09-03 21:21:21 +0100

    solved the location page problem

--- real_time_vehicle_monitoring_and_tracking_app/script.js
+++ real_time_vehicle_monitoring_and_tracking_app/script.js
@@ -1,21 +1,21 @@
 displayLocation = () => {
-    fetch('.\\locations.json')
+    fetch('locations.json')
     .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        appendData(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
+      return response.json();
+    })
+    .then(function (data) {
+      appendData(data);
+    })
+    .catch(function (err) {
+      console.log('Error: ' + err);
+    });
 }
 
 function appendData(data) {
-    var mainContainer = document.getElementById("locations");
+    var mainContainer = document.getElementById("myLocation");
     for (var i = 0; i < data.length; i++) {
-      var button = document.createElement("button");
+      var div = document.createElement("div");
       div.innerHTML = 'Location: ' + data[i].latitude+ ' ' + data[i].longitude;
-      mainContainer.appendChild(button);
+      mainContainer.appendChild(div);
     }
   }
---
+++ real_time_vehicle_monitoring_and_tracking_app/scripts/fetchLocation.js
@@ -0,0 +1,23 @@
+// Changed function to Immediately Invoked Function Expression(IIFE)
+const displayLocation = (() => {
+  fetch('.\\locations.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      appendData(data);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
+
+  function appendData(data) {
+    var mainContainer = document.querySelector("#locations ul");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("li");
+      div.innerHTML = 'Location: ' + data[i].latitude + ' ' + data[i].longitude;
+      mainContainer.appendChild(div);
+    }
+  }
+})()
