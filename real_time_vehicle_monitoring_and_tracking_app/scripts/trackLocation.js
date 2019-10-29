de12514a72d4ee3485a3c1a21c675ada7c5af5f5
Khadija Ladan <khadijaladan67@gmail.com>
2019-10-29 16:13:05 +0100

    added functions for tracking page

--- real_time_vehicle_monitoring_and_tracking_app/scripts/trackLocation.js
+++ real_time_vehicle_monitoring_and_tracking_app/scripts/trackLocation.js
@@ -1,4 +1,69 @@
-var myMap = L.map('mapId').setView([9.6183,6.5470], 13);
+ const displayLocation = (() => {
+    fetch('.\\locations.json')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        appendData(data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  
+  
+    function appendData(data) {
+        const chartData = data.map(data => {
+            return {
+                x: data.latitude,
+                y: data.longitude
+            };
+        });
 
-L.tileLayer('./images/map (2).png').addTo(myMap);
-L.geoJSON(nig).addTo(myMap);
+
+        
+        var chr = new Chart(document.getElementById("myChart"),{
+            type: 'scatter',
+
+            data: {
+                datasets: [{
+                    label: "TRACKING",
+                    data: chartData,
+                    showLine: true,
+                    fill: false,
+                    pointBackgroundColor: 'rgb(128, 128, 0)',
+                    borderColor: 'rgb(34, 110, 196)',
+                    scales: {
+                        xAxes: [{
+                            ticks:{
+                                max: 50,
+                                min: 0
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                max: 10,
+                                min: 0
+                            }
+                        }]
+                    }
+                }]
+            },
+
+            options: {
+                legend:{
+                    display: false
+                },
+
+                scales:{
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        display: false
+                    }]
+                }
+            }
+        });
+
+    }
+  })();
