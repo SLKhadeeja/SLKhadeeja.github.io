dbb14ee7e80fa207b4309a29bfafd05e4a70e4f6
Khadija Ladan <khadijaladan67@gmail.com>
2019-11-02 18:03:18 +0100

    solved violation message

--- real_time_vehicle_monitoring_and_tracking_app/scripts/lock.js
+++ real_time_vehicle_monitoring_and_tracking_app/scripts/lock.js
@@ -1,22 +1,25 @@
 //  Get element with ID of distance
-const distance = document.querySelector('#distance');
 
-const geoLibDistance = '0KM';
-    distance.innerHTML = geoLibDistance;
-    navigator.geolocation.watchPosition((position) => {    
-        const {longitude, latitude} = position.coords;
-        const myLocation = {
-            latitude,
-            longitude
-        };
-        const hisLocation = {
-            latitude: 6.057135999999999,
-            longitude: 7.4702848
-        };
-        console.log(myLocation);
-        const geoLibDistance = `${(geolib.getDistance(myLocation, hisLocation)/1000).toFixed(2)}KM`;
+const distance = () => {
+    const distance = document.querySelector('#distance');
+
+    const geoLibDistance = '0KM';
         distance.innerHTML = geoLibDistance;
-    }, () => {
-        alert('Please allow the app to get your location.');
-    }
-);
+        navigator.geolocation.getCurrentPosition((position) => {    
+            const {longitude, latitude} = position.coords;
+            const myLocation = {
+                latitude,
+                longitude
+            };
+            const hisLocation = {
+                latitude: 10.057135999999999,
+                longitude: 7.4702848
+            };
+            console.log(myLocation);
+            const geoLibDistance = `${(geolib.getDistance(myLocation, hisLocation)/1000).toFixed(2)}KM`;
+            distance.innerHTML = geoLibDistance;
+        }, () => {
+            alert('Please allow the app to get your location.');
+        }
+    );
+    };    
