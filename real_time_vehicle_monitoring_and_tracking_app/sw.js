ec3b68828000482cde9100bef6e942c47b3904c8
Khadija Ladan <khadijaladan67@gmail.com>
2019-11-04 18:00:36 +0100

    implemented service worker

--- real_time_vehicle_monitoring_and_tracking_app/sw.js
+++ real_time_vehicle_monitoring_and_tracking_app/sw.js
@@ -1,44 +1,39 @@
 self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('the-magic-cache').then(function(cache) {
-      })
-    );
-  });
+  e.waitUntil(
+    caches.open('autotrack').then(function(cache) {
+     return cache.addAll(
+       [
+        '/',
+        '/index.html',
+        '/locations.html',
+        '/locking.html',
+        '/tracking.html',
+        '/manifest.json',
+        '/locations.json',
+        '/images/background11.jpg',
+        '/images/logo.jpg',
+        '/images/phone.png',
+        '/images/track.png',
+        '/images/vehicle.png',
+        '/scripts/Chart.js',
+        '/scripts/fetchLocation.js',
+        '/scripts/geolib.js',
+        '/scripts/lock.js',
+        '/scripts/trackLocation.js',
+        '/stylesheets/main.css',
+        '/stylesheets/Chart.css'
+      ]
+     );
+   })
+ );
+});
 
-  self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('the-magic-cache').then(function(cache) {
-        return cache.addAll([
-          '../',
-          '../index.html',
-          '../locations.html',
-          '../locking.html',
-          '../tracking.html',
-          '../manifest.json',
-          '../locations.json',
-          '../images/background11.jpg',
-          '../images/logo.jpg',
-          '../images/phone.png',
-          '../images/track.png',
-          '../images/vehicle.png',
-          '../scripts/app.js',
-          '../scripts/Chart.js',
-          '../scripts/fetchLocation.js',
-          '../scripts/geolib.js',
-          '../scripts/lock.js',
-          '../scripts/trackLocation.js',
-          '../stylesheet/main.css',
-          '../stylesheet/Chart.css'
-        ]);
-      })
-    );
-  });
-
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+ });
