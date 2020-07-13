a92f9412129d16968e7043e9526d54898fe48524
Khadija Ladan <khadijaladan67@gmail.com>
2020-07-13 14:11:43 +0100

    error messages

--- react-form/src/components/form.js
+++ react-form/src/components/form.js
@@ -1,99 +1,149 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import '../styles/form.css';
 
 class Form extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        alert("Your registration has been successfully submitted")
+    let isEmailValid = false;
+    let isPasswordValid = false;
+
+    // Check if email contains any text before doing the email check
+    if (this.isFilled(email.value)) {
+        isEmailValid = this.validateEmail(email);
+    } else {
+        email.classList.add('error');
+        email.nextSibling.textContent = 'This field is required';
     }
 
-    validateEmail = (emailField)=> {
-        var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-        if (reg.test(emailField.value)) 
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-        }
-
-    validatePassword = (password)=> {
-        var reg = /^[A-Za-z]\w{7,14}$/;
-
-        if (reg.test(password.value)) 
-        {
-           return true; 
-        }
-        else{
-            alert('Password must contain lowercase, uppercase and numbers');
-            return false;
-        }
-        }
-
-    // errorMessage = () => {
-    //     input.setCustomValidity('This field is required')
-    // }
-
-
-    render() {
-        return(
-            <div className="form" onSubmit={this.handleSubmit.bind(this)} noValidate >
-                <form className="main-form" >
-                    <div className="name">
-
-                        <label className="input-field" id="firstName">
-                            First Name 
-                            <input type="text" required="required" placeholder="First Name"
-                                
-                                />
-                        
-                        </label>
-                        
-                        <label className="input-field" id="lastName">
-                            Last Name 
-                            <input type="text" required="required" placeholder="Last Name" />
-                        </label>
-                    </div>
-                    
-                    <div className="field">
-                        <label className="input-field">
-                            Email 
-                            <input type="email" required="required"  placeholder="Email" onChange={this.validateEmail.bind(this)} />
-                        </label>
-                        
-                        <label className="input-field">
-                            Password 
-                            <input type="password" required="required" placeholder="Password" onChange={this.validatePassword.bind(this)} />
-                        </label>
-                        
-                        <label className="input-field">
-                            Confirm Password 
-                            <input type="password" required="required" placeholder="Confirm Password" onChange={this.validatePassword.bind(this)} />
-                        </label>
-                        
-                        <label className="input-field">
-                            Website 
-                            <input type="text" required="required" placeholder="Website" />
-                        </label>
-                    </div>
-                    
-                    <div className="terms">
-                        <input type="checkbox" id="checkbox" required="required" />
-                        <p>I accept the terms and conditions</p>
-                    </div>
-                    
-                    <button type="submit" className="a-button" >
-                                Submit
-                    </button>
-                        
-                </form>
-            </div>
-
-        );
+    // Check if password contains any text before doing the email check
+    if (this.isFilled(email.value)) {
+        isPasswordValid = this.validatePassword(password);
+    } else {
+        password.classList.add('error');
+        password.nextSibling.textContent = 'This field is required';
     }
+
+
+    if (this.isFormInvalid([isEmailValid, isPasswordValid])) {
+      console.log('object');
+    } else {
+        alert('Your registration has been successfully submitted');
+    }
+  };
+
+  isFormInvalid = (validations) => {
+    // You'll pass all the check here and if any of the parameters is false, return false,
+    // else return true;
+    const notValid = validations.some((validation) => {
+      return validation === false;
+    });
+
+    return notValid;
+  };
+
+  isFilled = (field) => {
+      return field.length > 0;
+  }
+
+  validateEmail = (emailField) => {
+    var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (reg.test(emailField.value)) {
+        emailField.classList.remove('error');
+        emailField.nextSibling.textContent = '';
+      return true;
+    } else {
+        emailField.classList.add('error');
+        emailField.nextSibling.textContent = 'Email is not valid';
+      return false;
+    }
+  };
+
+  validatePassword = (password) => {
+    var reg = /^[A-Za-z]\w{7,14}$/;
+
+    if (reg.test(password.value)) {
+        password.classList.remove('error');
+        password.nextSibling.textContent = '';
+      return true;
+    } else {
+      password.classList.add('error');
+      password.nextSibling.textContent = 'Password is not valid';
+      return false;
+    }
+  };
+
+  // errorMessage = () => {
+  //     input.setCustomValidity('This field is required')
+  // }
+
+  render() {
+    return (
+      <div className='form' onSubmit={this.handleSubmit.bind(this)} noValidate>
+        <form className='main-form'>
+          <div className='name'>
+            <label className='input-field' id='firstName'>
+              First Name
+              <input type='text' placeholder='First Name' />
+            </label>
+
+            <label className='input-field' id='lastName'>
+              Last Name
+              <input type='text' placeholder='Last Name' />
+            </label>
+          </div>
+
+          <div className='field'>
+            <label className='input-field'>
+              Email
+              <input
+                id='email'
+                type='email'
+                placeholder='Email'
+              />
+              <p></p>
+            </label>
+
+            <label className='input-field'>
+              Password
+              <input
+                id="password"
+                type='password'
+                placeholder='Password'
+              />
+              <p></p>
+            </label>
+
+            <label className='input-field'>
+              Confirm Password
+              <input
+                type='password'
+                placeholder='Confirm Password'
+                onChange={this.validatePassword.bind(this)}
+              />
+            </label>
+
+            <label className='input-field'>
+              Website
+              <input type='text' placeholder='Website' />
+            </label>
+          </div>
+
+          <div className='terms'>
+            <input type='checkbox' id='checkbox' />
+            <p>I accept the terms and conditions</p>
+          </div>
+
+          <button type='submit' className='a-button'>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
